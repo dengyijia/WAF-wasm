00525a3e3c0ac70886b49ec54de3d1866ae6b04a
Adela Deng <kagura@google.com>
2020-08-21 20:57:51 +0000

    tests

--- integration_test/http-client-server.go
+++ integration_test/http-client-server.go
@@ -13,18 +13,16 @@ import (
 /*****************************
 * INTEGRATION TEST FRAMEWORK *
 ******************************/
-/*
-* type aliases for request components
-*/
-type Header = map[string]string
-type Cookie = map[string]string
-type Body = map[string]string
+
+type Map = map[string]string
 
 /*
-* Test struct
+* TestSuite struct
 *   keep track of info used by a test suite
+*   different proxy configurations corresponds
+*   to different test suites
 */
-type Test struct {
+type TestSuite struct {
   // url of the proxy to be tested
   // the test client will send requests to this url
   proxy_url string
@@ -36,43 +34,110 @@ type Test struct {
   // the request has been successful
   server_message string
 
-  // the channel for communication between the server
-  // and the client. Received requests will be pushed through this
-  // channel by the server for the client to verify
+  // the server that will be responding requests from the proxy
+  server http.Server
+
+  // the channels for communication between the server
+  // and the client. Received requests will be pushed through these
+  // channels by the server for the client to verify
   request_received chan *http.Request
   body_string_received chan string
+
+  // a collection of tests to run in this suite
+  // each test has a test name associated with it
+  tests map[string]Test
 }
 
 /*
-* Start the test server
-*   upon receiving a request, the server will push the request
-*   through the communication channel for the client to verify
-*   (since the request body will be closed when the client receive
-*   from the channel, the body is sent in a separate channel as a
-*   string literal)
-*
-*   the server will also send a response with server_message as its
-*   body
+* Test struct
+*   keep track of info used by one test in a test suite
 */
-func (t Test) StartServer() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+type Test struct {
+  // the test request to be made
+  path string
+  pathParams url.Values
+  headers Map
+  cookies Map
+  body  url.Values
+
+  // the expected sqli detection result for the proxy
+  // if true, the request contains sqli
+  sqli bool
+}
+
+/*
+* Initiate a new test suite with a given proxy_url
+*/
+func NewTestSuite(proxy_url string) TestSuite {
+  return TestSuite{
+    proxy_url: proxy_url,
+    server_port: ":8080",
+    server_message: "request received",
+    request_received: make(chan *http.Request),
+    body_string_received: make(chan string),
+    tests: map[string]Test{},
+  }
+}
+
+/*
+* Run all tests in a test suite
+*/
+func (t TestSuite) RunSuite() {
+  t.StartServer()
+  t.CheckProxyConnection()
+  for name, test := range t.tests {
+    t.RunTest(name, test)
+  }
+  t.CloseServer()
+}
+
+/*
+* Run a given test in a test suite
+* assuming the proxy has been running
+*/
+func (t TestSuite) RunTest(test_name string, test Test) {
+  result := t.Eval(test)
+  if !result {
+    fmt.Println("TEST FAILED: " + test_name)
+    fail()
+  }
+  fmt.Println("TEST PASSED: " + test_name)
+}
+
+/*
+* Initialize and Start the test server
+*/
+func (suite TestSuite) StartServer() {
+  // set path handler function
+  serveMux := http.NewServeMux()
+  serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
     body, _ := ioutil.ReadAll(r.Body)
     go func() {
-      t.request_received <- r
-    }()
+      suite.request_received <- r
+    } ()
     go func() {
-      t.body_string_received <- string(body)
-    }()
-    fmt.Fprintf(w, t.server_message)
+      suite.body_string_received <- string(body)
+    } ()
+    fmt.Fprintf(w, suite.server_message)
   })
-  go http.ListenAndServe(t.server_port, nil)
+
+  // start server
+  suite.server = http.Server {
+    Addr: suite.server_port,
+    Handler: serveMux,
+  }
+  go suite.server.ListenAndServe()
   fmt.Println("Server started")
+}
+
+func (t TestSuite) CloseServer() {
+  t.server.Close()
 }
 
 /*
 * Test if the proxy and the test server has been properly connected
 */
-func (t Test) CheckProxyConnection() {
+func (t TestSuite) CheckProxyConnection() {
   // wait and test that the proxy is up
   time.Sleep(2 * time.Second)
   response, err := http.Get(t.proxy_url)
@@ -91,34 +156,28 @@ func (t Test) CheckProxyConnection() {
   }
   <-t.request_received
   <-t.body_string_received
+
   fmt.Println("Client connected to server through proxy")
 }
 
 /*
-* test()
-*   test if a given request is handled properly by the proxy
-* Input:
-*   path, header, cookie, body: components of the request to be made
-*   sqli: true if the request contains a sql injection, false if not
-*   proxy_url: url of the proxy to send request to
-*   request_received: a channel shared with the server, the request received
-*     by the server will be pushed to this channel
-* Output:
-*   true if the test passed, false if not
+* evaluate if a given request is handled properly by the proxy
+* true if the test passed, false if not
 */
-func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli bool) bool {
+func (suite TestSuite) Eval(test Test) bool {
   // initialize request
-  request_body_map := url.Values{}
-  for key, val := range body {
-    request_body_map.Set(key, val)
-  }
-  request_body := request_body_map.Encode()
-  request, _ := http.NewRequest("POST", t.proxy_url + path, strings.NewReader(request_body))
+  request_body := test.body.Encode()
+  request_pathParams := test.pathParams.Encode()
+  request, _ := http.NewRequest(
+    "POST",
+    suite.proxy_url + test.path + "?" + request_pathParams,
+    strings.NewReader(request_body),
+  )
   request.Header.Set("content-type", "application/x-www-form-urlencoded")
-  for key, val := range headers {
+  for key, val := range test.headers {
     request.Header.Set(key, val)
   }
-  for key, val := range cookies {
+  for key, val := range test.cookies {
     request.AddCookie(&http.Cookie{Name: key, Value: val})
   }
 
@@ -135,16 +194,16 @@ func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli 
   var received *http.Request
   var received_body string
   select {
-    case received = <-t.request_received:
+    case received = <-suite.request_received:
     default: received = nil
   }
   select {
-    case received_body = <-t.body_string_received:
+    case received_body = <-suite.body_string_received:
     default: received_body = ""
   }
 
   // verify the request and the response
-  if sqli {
+  if test.sqli {
     // if the request contains SQL injection, it should be blocked
     // the server should never receive any request
     if received != nil {
@@ -166,13 +225,13 @@ func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli 
       fmt.Println("Request without SQL injection was blocked")
       return false
     }
-    if path != received.URL.Path {
+    if test.path != received.URL.Path {
       fmt.Println("Request without SQL injection has path altered by proxy")
-      fmt.Println("EXPECTED: ", path)
-      fmt.Println("ACTUAL: ", received.Header.Get(":path"))
+      fmt.Println("EXPECTED: ", test.path)
+      fmt.Println("ACTUAL: ", received.URL.Path)
       return false
     }
-    for key, val := range headers {
+    for key, val := range test.headers {
       if received.Header.Get(key) != val {
         fmt.Println("Request without SQL injection has header altered by proxy")
 	fmt.Println("EXPECTED: ", key, " -> ",  val)
@@ -180,7 +239,7 @@ func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli 
         return false
       }
     }
-    for key, val := range cookies {
+    for key, val := range test.cookies {
       cookie_sent, err := received.Cookie(key)
       if err != nil || cookie_sent.Value != val {
 	fmt.Println("Request without SQL injection has cookie altered by proxy")
@@ -197,7 +256,7 @@ func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli 
 
     // the client should receive a response from the server with status 200 Okay
     response_body, _ := ioutil.ReadAll(response.Body)
-    if response.StatusCode != 200 || string(response_body) != t.server_message {
+    if response.StatusCode != 200 || string(response_body) != suite.server_message {
       fmt.Println("Request without SQL did not receive 200 reply")
       return false
     }
@@ -213,76 +272,5 @@ func (t Test) Test(path string, headers Header, cookies Cookie, body Body, sqli 
 func fail() {
   fmt.Println("====== Integration tests failed ======")
   os.Exit(0)
-}
-
-func run(test_name string, result bool) {
-  if !result {
-    fmt.Println("TEST FAILED: " + test_name)
-    fail()
-  }
-  fmt.Println("TEST PASSED: " + test_name)
-}
-
-/*************
-* TEST CASES *
-**************/
-func (test Test) TestRequestWithoutSQLI() bool {
-  return test.Test("/",
-           Header{
-             "header-key-0": "header-value-0",
-	   },
-	   Cookie{
-	     "cookie-name-0": "cookie-value-0",
-	   },
-	   Body{
-             "body-key-0": "body-value-0",
-	   },
-           false)
-}
-
-func (test Test) TestSQLIinPath() bool {
-  return test.Test("/path?val=-1%27+and+1%3D1%0D%0A",
-           Header{},
-	   Cookie{},
-	   Body{},
-           true)
-
-}
-
-func TestSQLIinHeader() {
-}
-
-func TestSQLIinCookie() {
-}
-
-func TestSQLIinBody() {
-}
-
-func TestSQLIExcludedByConfig() {
-}
-
-func main() {
-  // begin testing
-  fmt.Println("====== Integration test starting ======")
-  request_received := make(chan *http.Request)
-  body_string_received := make(chan string)
-
-  // set up test suite
-  test := Test{
-    proxy_url: "http://proxy:8000",
-    server_port: ":8080",
-    server_message: "request received",
-    request_received: request_received,
-    body_string_received: body_string_received,
-  }
-  test.StartServer()
-  test.CheckProxyConnection()
-
-  // start testing
-  run("Test Request Without SQLI", test.TestRequestWithoutSQLI())
-  run("Test SQLI in path", test.TestSQLIinPath())
-
-  // terminate with sucess
-  fmt.Println("====== Passed all integration tests ======")
 }
 
