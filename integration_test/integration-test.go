f89b6330fe8f9137369359067fe79d8c7816e79b
Adela Deng <kagura@google.com>
2020-08-28 16:13:32 +0000

    reformat go code with camel case variables and gofmt

--- integration_test/integration-test.go
+++ integration_test/integration-test.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/url"
-	"time"
-	"strings"
 	"os"
+	"strings"
+	"time"
 )
 
 /*****************************
@@ -21,18 +21,18 @@ type Map = map[string]string
 *   keep track of info used by a test suite
 *   different proxy configurations corresponds
 *   to different test suites
-*/
+ */
 type TestSuite struct {
 	// url of the proxy to be tested
 	// the test client will send requests to this url
-	proxy_url string
+	proxyURL string
 
 	// the port that the test server will be listening at
-	server_port string
+	serverPort string
 
 	// the expected message received from the test server if
 	// the request has been successful
-	server_message string
+	serverMessage string
 
 	// the server that will be responding requests from the proxy
 	server http.Server
@@ -40,8 +40,8 @@ type TestSuite struct {
 	// the channels for communication between the server
 	// and the client. Received requests will be pushed through these
 	// channels by the server for the client to verify
-	request_received chan *http.Request
-	body_string_received chan string
+	requestReceived    chan *http.Request
+	bodyStringReceived chan string
 
 	// a collection of requests to run in this suite
 	// each test has a test name associated with it
@@ -51,98 +51,98 @@ type TestSuite struct {
 /*
 * TestRequest struct
 *   keep track of info used by one request in a test suite
-*/
+ */
 type TestRequest struct {
 	// the test request to be made
-	path string
+	path       string
 	pathParams url.Values
-	headers Map
-	cookies Map
-	body  url.Values
+	headers    Map
+	cookies    Map
+	body       url.Values
 
 	// the expected sqli detection result for the proxy
 	// if true, the request contains sqli
-	sqli bool
+	sqli      bool
 	sqli_body bool
 }
 
 /*
-* Initiate a new test suite with a given proxy_url
-*/
-func NewTestSuite(proxy_url string) TestSuite {
+* Initiate a new test suite with a given proxyURL
+ */
+func NewTestSuite(proxyURL string) TestSuite {
 	return TestSuite{
-		proxy_url: proxy_url,
-		server_port: ":8080",
-		server_message: "request received",
-		request_received: make(chan *http.Request),
-		body_string_received: make(chan string),
-		requests: map[string]TestRequest{},
+		proxyURL:           proxyURL,
+		serverPort:         ":8080",
+		serverMessage:      "request received",
+		requestReceived:    make(chan *http.Request),
+		bodyStringReceived: make(chan string),
+		requests:           map[string]TestRequest{},
 	}
 }
 
 /*
 * Run all requests in a test suite
-*/
-func (t TestSuite) RunRequests() {
-	t.StartServer()
-	t.CheckProxyConnection()
-	for name, test := range t.requests {
-		t.RunTestRequest(name, test)
+ */
+func (suite TestSuite) RunRequests() {
+	suite.StartServer()
+	suite.CheckProxyConnection()
+	for name, test := range suite.requests {
+		suite.RunTestRequest(name, test)
 	}
-	t.CloseServer()
+	suite.CloseServer()
 }
 
 /*
 * Initiate a new test request with default content
-*/
+ */
 func NewTestRequest() TestRequest {
 	return TestRequest{
-		path: "/",
+		path:       "/",
 		pathParams: url.Values{"path-key-0": {"path-val-0"}},
-		headers: Map{"header-key-0": "header-val-0"},
-		cookies: Map{"cookie-key-0": "cookie-val-0"},
-		body: url.Values{"body-key-0": {"body-val-0"}},
-		sqli: false,
-		sqli_body: false,
+		headers:    Map{"header-key-0": "header-val-0"},
+		cookies:    Map{"cookie-key-0": "cookie-val-0"},
+		body:       url.Values{"body-key-0": {"body-val-0"}},
+		sqli:       false,
+		sqli_body:  false,
 	}
 }
 
 /*
 * Run a given test in a test suite
 * assuming the server and the proxy has been running
-*/
-func (t TestSuite) RunTestRequest(test_name string, test TestRequest) {
-	result := t.Eval(test)
+ */
+func (suite TestSuite) RunTestRequest(testName string, test TestRequest) {
+	result := suite.Eval(test)
 	if !result {
-		fmt.Println("TEST FAILED: " + test_name)
+		fmt.Println("TEST FAILED: " + testName)
 		fail()
 	}
-	fmt.Println("TEST PASSED: " + test_name)
+	fmt.Println("TEST PASSED: " + testName)
 }
 
 /*
 * Initialize and Start the test server
-*/
+ */
 func (suite TestSuite) StartServer() {
 	// set path handler functions
 	serveMux := http.NewServeMux()
 	serveMux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		body, _ := ioutil.ReadAll(r.Body)
 		go func() {
-			suite.request_received <- r
-		} ()
+			suite.requestReceived <- r
+		}()
 		go func() {
-			suite.body_string_received <- string(body)
-		} ()
-		fmt.Fprintf(w, suite.server_message)
+			suite.bodyStringReceived <- string(body)
+		}()
+		fmt.Fprintf(w, suite.serverMessage)
 	})
 	serveMux.HandleFunc("/shutdown", func(w http.ResponseWriter, r *http.Request) {
 		suite.server.Close()
 	})
 
 	// start server
-	suite.server = http.Server {
-		Addr: suite.server_port,
+	suite.server = http.Server{
+		Addr:    suite.serverPort,
 		Handler: serveMux,
 	}
 	go func() {
@@ -151,63 +151,63 @@ func (suite TestSuite) StartServer() {
 			fmt.Println("Server shutdown unexpectedly:", err)
 			fail()
 		}
-	} ()
+	}()
 	fmt.Println("Server started")
 }
 
 /*
 * Close the test server
-*/
-func (t TestSuite) CloseServer() {
-	_, err := http.Get(t.proxy_url + "/shutdown")
+ */
+func (suite TestSuite) CloseServer() {
+	_, err := http.Get(suite.proxyURL + "/shutdown")
 	if err != nil {
 		fmt.Println("Server shutdown failed:", err)
 		fail()
 	}
-	close(t.request_received)
-	close(t.body_string_received)
+	close(suite.requestReceived)
+	close(suite.bodyStringReceived)
 }
 
 /*
 * Test if the proxy and the test server has been properly connected
-*/
-func (t TestSuite) CheckProxyConnection() {
+ */
+func (suite TestSuite) CheckProxyConnection() {
 	// wait and test that the proxy is up
 	fmt.Println("Check proxy connection")
 	time.Sleep(1 * time.Second)
-	response, err := http.Get(t.proxy_url)
+	response, err := http.Get(suite.proxyURL)
 	if err != nil {
 		fmt.Println(err)
 		fmt.Println("Client connecting to proxy ...")
-		t.CheckProxyConnection()
+		suite.CheckProxyConnection()
 		return
 	}
 	defer response.Body.Close()
 
 	// check that proxy reply indeed comes from server
-	response_body, _ := ioutil.ReadAll(response.Body)
-	if response.StatusCode != 200 || string(response_body) != t.server_message {
+	responseBody, _ := ioutil.ReadAll(response.Body)
+	if response.StatusCode != 200 || string(responseBody) != suite.serverMessage {
 		fmt.Println("Proxy connection failed")
-		fmt.Println("Received response", response.Status, string(response_body))
+		fmt.Println("Received response", response.Status, string(responseBody))
 		fail()
 	}
-	<-t.request_received
-	<-t.body_string_received
+	<-suite.requestReceived
+	<-suite.bodyStringReceived
 	fmt.Println("Client connected to server through proxy")
 }
 
 /*
 * evaluate if a given request is handled properly by the proxy
 * true if the test passed, false if not
-*/
+ */
 func (suite TestSuite) Eval(test TestRequest) bool {
 	// initialize request
-	request_body := test.body.Encode()
-	request_pathParams := test.pathParams.Encode()
+	requestBody := test.body.Encode()
+	requestPathParams := test.pathParams.Encode()
 	request, _ := http.NewRequest(
 		"POST",
-		suite.proxy_url + test.path + "?" + request_pathParams,
-		strings.NewReader(request_body),
+		suite.proxyURL+test.path+"?"+requestPathParams,
+		strings.NewReader(requestBody),
 	)
 	request.Header.Set("content-type", "application/x-www-form-urlencoded")
 	for key, val := range test.headers {
@@ -232,14 +232,16 @@ func (suite TestSuite) Eval(test TestRequest) bool {
 
 	// get request from server
 	var received *http.Request
-	var received_body string
+	var receivedBody string
 	select {
-	case received = <-suite.request_received:
-		default: received = nil
+	case received = <-suite.requestReceived:
+	default:
+		received = nil
 	}
 	select {
-	case received_body = <-suite.body_string_received:
-		default: received_body = ""
+	case receivedBody = <-suite.bodyStringReceived:
+	default:
+		receivedBody = ""
 	}
 
 	// verify the request and the response
@@ -248,9 +250,9 @@ func (suite TestSuite) Eval(test TestRequest) bool {
 		// if the SQL injection is in body, the server should receive request with empty body
 		// if the SQL injection is in header, the server should never receive any request
 		if test.sqli_body {
-			if received_body != "" {
+			if receivedBody != "" {
 				fmt.Println("Request with SQL injection in body was not blocked")
-				fmt.Println(received_body)
+				fmt.Println(receivedBody)
 				return false
 			}
 		} else if received != nil {
@@ -281,29 +283,29 @@ func (suite TestSuite) Eval(test TestRequest) bool {
 		for key, val := range test.headers {
 			if received.Header.Get(key) != val {
 				fmt.Println("Request without SQL injection has header altered by proxy")
-				fmt.Println("EXPECTED: ", key, " -> ",  val)
+				fmt.Println("EXPECTED: ", key, " -> ", val)
 				fmt.Println("ACTUAL: ", key, " -> ", received.Header.Get(key))
 				return false
 			}
 		}
 		for key, val := range test.cookies {
-			cookie_sent, err := received.Cookie(key)
-			if err != nil || cookie_sent.Value != val {
+			cookieSent, err := received.Cookie(key)
+			if err != nil || cookieSent.Value != val {
 				fmt.Println("Request without SQL injection has cookie altered by proxy")
-				fmt.Println("EXPECTED: ", cookie_sent)
+				fmt.Println("EXPECTED: ", cookieSent)
 				return false
 			}
 		}
-		if received_body != request_body {
+		if receivedBody != requestBody {
 			fmt.Println("Request without SQL injection has body altered by proxy")
-			fmt.Println("EXPECTED: ", request_body)
-			fmt.Println("ACTUAL: ", received_body)
+			fmt.Println("EXPECTED: ", requestBody)
+			fmt.Println("ACTUAL: ", receivedBody)
 			return false
 		}
 
 		// the client should receive a response from the server with status 200 Okay
-		response_body, _ := ioutil.ReadAll(response.Body)
-		if response.StatusCode != 200 || string(response_body) != suite.server_message {
+		responseBody, _ := ioutil.ReadAll(response.Body)
+		if response.StatusCode != 200 || string(responseBody) != suite.serverMessage {
 			fmt.Println("Request without SQL did not receive 200 reply")
 			return false
 		}
@@ -315,9 +317,8 @@ func (suite TestSuite) Eval(test TestRequest) bool {
 * fail()
 *   this function is called when a test failed
 *   print integration test failure message and abort the program
-*/
+ */
 func fail() {
 	fmt.Println("====== Integration tests failed ======")
 	os.Exit(0)
 }
-
--- integration_test/main.go
+++ integration_test/main.go
@@ -1,37 +1,38 @@
 package main
+
 import (
 	"fmt"
 )
 
 // Config struct reflects the expectations for the WASM filter configuration
 type Config struct {
-	config_name string
-	proxy_url string
+	configName string
+	proxyURL   string
 
 	// the headers that are expected to be included/excluded in detection
-	header_include []string
-	header_exclude []string
+	headerInclude []string
+	headerExclude []string
 
 	// true if the config for cookie contains `include`
-	// in this case, only the cookies specified in cookie_included will
+	// in this case, only the cookies specified in cookieIncluded will
 	// be included in detection
-	cookie_include_bool bool
+	cookieIncludeBool bool
 
 	// the cookie names that are expected to be included/excluded in detection
-	cookie_include []string
-	cookie_exclude []string
+	cookieInclude []string
+	cookieExclude []string
 
 	// true if the config for body contains `include`
-	body_include_bool bool
+	bodyIncludeBool bool
 
 	// the body param keys that are expected to be included/excluded in detection
-	body_include []string
-	body_exclude []string
+	bodyInclude []string
+	bodyExclude []string
 }
 
 func TestForConfig(config Config) {
-	suite := NewTestSuite(config.proxy_url)
-	sqli_string := "=1' AND 1=1"
+	suite := NewTestSuite(config.proxyURL)
+	sqliString := "=1' AND 1=1"
 	var request TestRequest
 
 	// no SQL injection
@@ -39,88 +40,88 @@ func TestForConfig(config Config) {
 
 	// path
 	request = NewTestRequest()
-	request.pathParams.Set("path-key-0", sqli_string)
+	request.pathParams.Set("path-key-0", sqliString)
 	request.sqli = true
 	suite.requests["Request with SQL injection in path value"] = request
 
 	request = NewTestRequest()
-	request.pathParams.Set(sqli_string, "path-val-0")
+	request.pathParams.Set(sqliString, "path-val-0")
 	request.sqli = true
 	suite.requests["Request with SQL injection in path key"] = request
 
 	// header
-	for _, key := range config.header_include {
+	for _, key := range config.headerInclude {
 		request = NewTestRequest()
-		request.headers[key] = sqli_string
+		request.headers[key] = sqliString
 		request.sqli = true
-		suite.requests["Request with SQL injection in included header: " + key] = request
+		suite.requests["Request with SQL injection in included header: "+key] = request
 	}
-	for _, key := range config.header_exclude {
+	for _, key := range config.headerExclude {
 		request = NewTestRequest()
-		request.headers[key] = sqli_string
+		request.headers[key] = sqliString
 		request.sqli = false
-		suite.requests["Request with SQL injection in excluded header: " + key] = request
+		suite.requests["Request with SQL injection in excluded header: "+key] = request
 	}
 
 	// cookie
-	for _, key := range config.cookie_include {
+	for _, key := range config.cookieInclude {
 		request = NewTestRequest()
-		request.cookies[key] = sqli_string
+		request.cookies[key] = sqliString
 		request.sqli = true
-		suite.requests["Request with SQL injection in included cookie: " + key] = request
+		suite.requests["Request with SQL injection in included cookie: "+key] = request
 	}
-	for _, key := range config.cookie_exclude {
+	for _, key := range config.cookieExclude {
 		request = NewTestRequest()
-		request.cookies[key] = sqli_string
+		request.cookies[key] = sqliString
 		request.sqli = false
-		suite.requests["Request with SQL injection in excluded cookie: " + key] = request
+		suite.requests["Request with SQL injection in excluded cookie: "+key] = request
 	}
-	if !config.cookie_include_bool {
+	if !config.cookieIncludeBool {
 		request = NewTestRequest()
-		request.cookies[sqli_string] = "value"
+		request.cookies[sqliString] = "value"
 		request.sqli = true
 		suite.requests["Request with SQL injection in cookie key"] = request
 	}
 
 	// body
-	for _, key := range config.body_include {
+	for _, key := range config.bodyInclude {
 		request = NewTestRequest()
-		request.body.Set(key, sqli_string)
+		request.body.Set(key, sqliString)
 		request.sqli = true
 		request.sqli_body = true
-		suite.requests["Request with SQL injection in included body param: " + key] = request
+		suite.requests["Request with SQL injection in included body param: "+key] = request
 	}
-	for _, key := range config.body_exclude {
+	for _, key := range config.bodyExclude {
 		request = NewTestRequest()
-		request.body.Set(key, sqli_string)
+		request.body.Set(key, sqliString)
 		request.sqli = false
-		suite.requests["Request with SQL injection in excluded body param: " + key] = request
+		suite.requests["Request with SQL injection in excluded body param: "+key] = request
 	}
-	if !config.body_include_bool {
+	if !config.bodyIncludeBool {
 		request = NewTestRequest()
-		request.body.Set(sqli_string, "value")
+		request.body.Set(sqliString, "value")
 		request.sqli = true
 		request.sqli_body = true
 		suite.requests["Request with SQL injection in body key"] = request
 	}
-	fmt.Println("------ Tests for ", config.config_name, "Config starts ------")
+	fmt.Println("------ Tests for ", config.configName, "Config starts ------")
 	suite.RunRequests()
-	fmt.Println("------ All tests for ", config.config_name, "Config passed ------")
+	fmt.Println("------ All tests for ", config.configName, "Config passed ------")
 }
 
 func TestDefault() {
 	// This test corresponds to the configuration in envoy-default.yaml
 	c := Config{
-		config_name: "Default",
-		proxy_url: "http://proxy-default:8081",
-		header_include: []string{"referer", "user-agent"},
-		header_exclude: []string{"header-key-1"},
-		cookie_include_bool: false,
-		cookie_include: []string{"cookie-key-1"},
-		cookie_exclude: []string{},
-		body_include_bool: false,
-		body_include: []string{"body-key-1"},
-		body_exclude: []string{},
+		configName:        "Default",
+		proxyURL:          "http://proxy-default:8081",
+		headerInclude:     []string{"referer", "user-agent"},
+		headerExclude:     []string{"header-key-1"},
+		cookieIncludeBool: false,
+		cookieInclude:     []string{"cookie-key-1"},
+		cookieExclude:     []string{},
+		bodyIncludeBool:   false,
+		bodyInclude:       []string{"body-key-1"},
+		bodyExclude:       []string{},
 	}
 	TestForConfig(c)
 }
@@ -128,16 +129,16 @@ func TestDefault() {
 func TestInclude() {
 	// This test corresponds to the configuration in envoy-include.yaml
 	c := Config{
-		config_name: "Include",
-		proxy_url: "http://proxy-include:8082",
-		header_include: []string{"header-key-0", "referer", "user-agent"},
-		header_exclude: []string{"header-key-1"},
-		cookie_include_bool: true,
-		cookie_include: []string{"cookie-key-0"},
-		cookie_exclude: []string{"cookie-key-1"},
-		body_include_bool: true,
-		body_include: []string{"body-key-0"},
-		body_exclude: []string{"body-key-1"},
+		configName:        "Include",
+		proxyURL:          "http://proxy-include:8082",
+		headerInclude:     []string{"header-key-0", "referer", "user-agent"},
+		headerExclude:     []string{"header-key-1"},
+		cookieIncludeBool: true,
+		cookieInclude:     []string{"cookie-key-0"},
+		cookieExclude:     []string{"cookie-key-1"},
+		bodyIncludeBool:   true,
+		bodyInclude:       []string{"body-key-0"},
+		bodyExclude:       []string{"body-key-1"},
 	}
 	TestForConfig(c)
 }
@@ -145,16 +146,16 @@ func TestInclude() {
 func TestExclude() {
 	// This test corresponds to the configuration in envoy-exclude.yaml
 	c := Config{
-		config_name: "Exclude",
-		proxy_url: "http://proxy-exclude:8083",
-		header_include: []string{"user-agent"},
-		header_exclude: []string{"header-key-0", "referer"},
-		cookie_include_bool: false,
-		cookie_include: []string{"cookie-key-1"},
-		cookie_exclude: []string{"cookie-key-0"},
-		body_include_bool: false,
-		body_include: []string{"body-key-1"},
-		body_exclude: []string{"body-key-0"},
+		configName:        "Exclude",
+		proxyURL:          "http://proxy-exclude:8083",
+		headerInclude:     []string{"user-agent"},
+		headerExclude:     []string{"header-key-0", "referer"},
+		cookieIncludeBool: false,
+		cookieInclude:     []string{"cookie-key-1"},
+		cookieExclude:     []string{"cookie-key-0"},
+		bodyIncludeBool:   false,
+		bodyInclude:       []string{"body-key-1"},
+		bodyExclude:       []string{"body-key-0"},
 	}
 	TestForConfig(c)
 }
